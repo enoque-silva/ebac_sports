56dd0460a4324d57753102bf40bc0e1e301e5e60
enoque <eu.enoqueribeiro@gmail.com>
2025-09-21 23:39:11 -0300

    criação do index.ts do Store

---
+++ src/store/index.ts
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit'
+import carrinhoReducer from './slices/carrinhoSlice'
+import favoritosReducer from './slices/favoritosSlice'
+
+const store = configureStore({
+  reducer: {
+    carrinho: carrinhoReducer,
+    favoritos: favoritosReducer
+  }
+})
+
+export type RootState = ReturnType<typeof store.getState>
+export type AppDispatch = typeof store.dispatch
