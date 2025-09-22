3bdbe7878e0d9cdbe122809b7a329c50b1ce8650
enoque <eu.enoqueribeiro@gmail.com>
2025-09-22 00:20:15 -0300

    criação do store/index.ts

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,11 +2,7 @@ import { configureStore } from '@reduxjs/toolkit'
 import carrinhoReducer from './slices/carrinhoSlice'
 import favoritosReducer from './slices/favoritosSlice'
 
-<<<<<<< HEAD
 export const store = configureStore({
-=======
-const store = configureStore({
->>>>>>> 56dd0460a4324d57753102bf40bc0e1e301e5e60
   reducer: {
     carrinho: carrinhoReducer,
     favoritos: favoritosReducer
